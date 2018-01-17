11ae96f063c2613d86005b44c18fbaa25372ef55
juhiautomation <mail2juhi.agrawal@gmail.com>
2018-01-17 23:17:42 +0530

    Star program completed

--- CustomString.java
+++ CustomString.java
@@ -1,5 +1,15 @@
 public class CustomString{
-public static void main(String args[]){
-System.out.println("juhi agrawal");
-}
+	public static void main(String args[]){
+		System.out.println("juhi agrawal");
+	
+		for(int i=0;i<4;i++){
+	   	   for(int k=3;k>i;k--){
+			System.out.print(" ");
+		   }
+		   for(int j=0;j<2*i+1;j++){
+			System.out.print("*");
+	   	   }
+           	System.out.println();
+		}
+	}
 }
